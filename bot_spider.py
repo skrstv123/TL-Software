f4f875c40d6e439e2af738404936b787ba811ff4
skrstv123 <47166055+skrstv123@users.noreply.github.com>
2019-03-22 22:25:53 +0530

    Update II bot spider.py

--- bot_spider.py
+++ bot_spider.py
@@ -13,5 +13,11 @@ class bot(scrapy.Spider):
     name = 'bot'
     allowed_domains = ["india.gov.in/topics/travel-tourism/approved-agents"]
     start_urls = ["https://www.india.gov.in/topics/travel-tourism/approved-agents"]
-
+    
+    def parse(self, response):
+		links=response.css('h3 a::attr(href)').extract();dd=dict()
+		for x in range(len(links)):
+			dd[x]=links[x]
+		
+		yield dd
   
